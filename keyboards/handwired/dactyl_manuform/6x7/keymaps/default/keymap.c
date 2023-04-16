b915946e7151803913cab704102d58a703a248bb
Aleksey Korolev <aking@live.ru>
2023-04-16 22:30:19 +0300

    stable

--- keyboards/handwired/dactyl_manuform/6x7/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x7/keymaps/default/keymap.c
@@ -12,19 +12,14 @@ enum custom_layer {
     _RAISE,
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    return false;
-}
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_6x7(
-        _______ , KC_GRV  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,    _______ , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_LBRC ,
-        _______ , KC_TAB  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    ,    _______ , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_QUOT ,
-        _______ , KC_LSFT , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    ,    _______ , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_LSFT ,
-        _______ , KC_LCTL , KC_Z    , KC_X    , KC_C    , KC_V    ,                        KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_LCTL ,
-        KC_ESC  , KC_F1   , KC_F2   , KC_F3   , KC_F4   ,                                            KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  ,
+        _______ , KC_GRV  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,    KC_A    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_A    , 
+        _______ , KC_TAB  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    ,    KC_B    , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_B    ,
+        _______ , KC_LSFT , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    ,    KC_C    , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_C    ,
+        _______ , KC_LCTL , KC_Z    , KC_X    , KC_C    , KC_V    ,                        KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_D    ,
+        KC_ESC  , KC_F1   , KC_F2   , KC_F3   , KC_F4   ,                                            KC_A    , KC_B    , KC_C    , KC_D    , KC_E    ,
 	                                        KC_A    , KC_B    , KC_C    ,    KC_Q    , KC_W    , KC_E    ,
 	                                        KC_D    , KC_E    , KC_F    ,    KC_R    , KC_T    , KC_Y
     )
-};
+}; 
