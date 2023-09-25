b9c96a53b5b5c4e97d54be79fc3525962014182c
Aleksey Korolev <aking@live.ru>
2023-09-25 11:10:41 +0300

    otree odired ofile

--- keyboards/handwired/dactyl_manuform/6x7/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x7/keymaps/default/keymap.c
@@ -4,28 +4,44 @@
 #include "action_layer.h"
 #include QMK_KEYBOARD_H
 #include "keydef.h"
+#include "win.h"
 #include "../../6x7.h" // чтобы clangd не ругался
 
 enum custom_keycodes {
-    TEST = SAFE_RANGE,
-    OSMETA,
-    APPSW,
+    TEST_ = SAFE_RANGE,
+    SYSMETA_,
+    APPSW_,
     ALTTAB,
     SALTTAB,
-    OSL_REF
+    REF__,
+    BRA__,
+    PARENS,
+    PARENSRU,
+    BRACES,
+    BRACESRU,
+    BRCKS,
+    BRCKSRU,
+    ABRCKS,
+    ABRCKSRU,
+    QUOTS,
+    QUOTSRU,
+    DQUOTS,
+    DQUOTSRU,
+    
 };
 
 enum custom_layer {
     __QWERTY,
     __OS,
     __APPSWITCH,
-    __REF
+    __REF,
+    __BRA
 };
 
-static const layer_state_t osl_mask = 0b1000u;
+static const layer_state_t osl_mask = 0b11000u;
 
 static const uint16_t timer_threshold = 250;
-static const uint16_t oneshot_threshold = 250;
+static const uint16_t oneshot_threshold = 1000;
 static uint16_t ostimer;
 static uint16_t oneshot_timer;
 
@@ -39,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         /*  BRACKS,           PARENS,  OSL_SYM, _O,      _EQL,    _MIN,             _BSP,    _G,      _C,      _R,      _F,      _K,      BRACES, _F3, */
         /*  OSL_NUM,  XDOT,   _A,      _E,      _I,      _U,               _L,      _H,      _T,      _N,      _S,      _RT,                    G(A(_ENT)), */
         /*  BSEL,     _A,     _J,      _Q,      _SLS,    _P,      _ESC,             _D,      _M,      _W,      _V,      _X,      _Z,      C(A(_Y)),KC_VOLU, */
-        /*  OSL_EDI,                   _LGUI,   RALT,             _SPC,OSM(MOD_LSFT),OSMETA,           RCMD,   RALT,OSM(MOD_LCTL),OSL_REF, OSL_HELP), */
+        /*  OSL_EDI,                   _LGUI,   RALT,             _SPC,OSM(MOD_LSFT),SYSMETA_,           RCMD,   RALT,OSM(MOD_LCTL),REF__, OSL_HELP), */
 
     /* [__OS] = LAYOUT_6x7( */
     /*     _______ , _______ , _______ , _______ , _______ , _______ , _______ ,    _______ , _______ , _______ , _______ , _______ , _______ , _______ ,  */
@@ -54,18 +70,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   
   [__QWERTY] = LAYOUT_6x7(
-        _______ , KC_GRV  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,    KC_A    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_A    , 
+	S(_BSL) , S(_BSL) , QUOTS   , S(_MIN) , _TAB    , S(_5)   , _SPC    ,    _SPC    , _______ , S(_2)   , _B      , _Y      , BRA__   , FINDNXT ,
 	_A      , _A      , _A      , _O      , _EQL    , _MIN    , _SPC    ,    _SPC    , _BSP    , _G      , _C      , _R      , _F      , _K      ,
 	_A      , _DOT    , _A      , _E      , _I      , _U      , _SPC    ,    _BSP    , _L      , _H      , _T      , _N      , _S      , _RT     ,
-        _______ , KC_LCTL , KC_Z    , KC_X    , KC_C    , KC_V    ,                        KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_D    ,
-        KC_ESC  , KC_F1   , KC_F2   , KC_F3   , KC_F4   ,                                            KC_A    , KC_B    , KC_C    , KC_D    , KC_E    ,
-	                                        KC_A    , KC_B    , KC_C    ,    KC_A    , OSMETA  , OSL_REF    ,
-	                                        KC_D    , KC_E    , KC_F    ,    KC_R    , KC_T    , KC_Y
+	_SPC    , _J      , _Q      , _SLS    , _P      , _ESC    ,                        _D      , _M      , _W      , _V      , _X      , _Z      ,
+        KC_ESC  , KC_F1   , KC_F2   , KC_F3   , KC_F4   ,                                            KC_A    , KC_B    , KC_C    , KC_D    , REF__   ,
+	                                        KC_A    , KC_B    , KC_C    ,    _ENT    , SYSMETA_, REF__   ,
+                                                KC_D    , KC_E    , KC_F    ,    _ESC    , _BSP    , BRA__
 			 ),
+
   [__OS] = LAYOUT_6x7(
         _______ , _______ , _______ , _______ , _______ , _______ , _______ ,    _______ , _______ , _______ , _______ , _______ , _______ , _______ , 
         _______ , _______ , _______ , _______ , _______ , _______ , _______ ,    _______ , _______ , _______ , _______ , _______ , _______ , _______ , 
-        _______ , _______ , _______ , _______ , _______ , _______ , _______ ,    _______ , _______ , APPSW   , _______ , _______ , _______ , _______ , 
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ ,    _______ , _______ , APPSW_  , _______ , _______ , _______ , _______ , 
         _______ , _______ , _______ , _______ , _______ , _______ ,                        _______ , _______ , _______ , _______ , _______ , _______ , 
         _______ , _______ , _______ , _______ , _______ ,                                            _______ , _______ , _______ , _______ , _______ , 
 	                                        _______ , _______ , _______ ,    _______ , _______ , _______ ,
@@ -86,10 +103,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_A      , _DOT    , _A      , _E      , _I      , _U      , _SPC    ,    _BSP    , _L      , _A      , _A      , _N      , _S      , _RT     ,
         _______ , KC_LCTL , KC_Z    , KC_X    , KC_C    , KC_V    ,                        KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_D    ,
         KC_ESC  , KC_F1   , KC_F2   , KC_F3   , KC_F4   ,                                            KC_A    , KC_B    , KC_C    , KC_D    , KC_E    ,
-	                                        KC_A    , KC_B    , KC_C    ,    KC_A    , OSMETA  , KC_E    ,
+	                                        KC_A    , KC_B    , KC_C    ,    KC_A    , SYSMETA_  , KC_E    ,
 	                                        KC_D    , KC_E    , KC_F    ,    KC_R    , KC_T    , KC_Y
-    )
+			 ),
+  [__BRA] = LAYOUT_6x7(
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ ,    _______ , _______ , _______ , _______ , _______ , _______ , _______ , 
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ ,    _______ , _______ , OTREE   , ODIRED  , OFILE   , _______ , _______ , 
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ ,    _______ , C(_L)   , C(S(_P)), _______ , _______ , _______ , _______ , 
+        _______ , _______ , _______ , _______ , _______ , _______ ,                        _______ , _______ , _______ , _______ , _______ , _______ , 
+        _______ , _______ , _______ , _______ , _______ ,                                            _______ , _______ , _______ , _______ , _______ , 
+	                                        _______ , _______ , _______ ,    _______ , _______ , _______ ,
+	                                        _______ , _______ , _______ ,    _______ , _______ , _______
+		     ),
+        /* LAYOUT_all //%% oneshot:bra */
+        /* (_______,          S(_1),   CCS,     S(_LBR), BRACES,  S(_RBR), _______, _______, FINDAT,  QUOTES,  _COM,    OCLIP,   _______, _______, _______, */
+        /*  _______,          S(_9),   S(_0),   ABRACKS, S(_COM), S(_DOT),          ORC,     OTREE,   OCWD,    ORECENT, OHOME,   ODIRED,  _______, _______, */
+        /*  _______,          _LBR,    _RBR,    E_CC,    CCS,     HELPKEY,          PALETTE, MX,      OHOME,   _______, S(_4),   S(_3),            _______, */
+        /*  _______, _______, DEREF,   E_CV,    E_CX,    ORC,     _F1,              LCTL(_R),CTA(_S), LCTL(_W),S(_8)   ,S(_SLS), E_CZ,    _______, _______, */
+        /*  _______,                   _______, _______,          COM_SPC, ORC,     COM_ENT,          _______, _______,          _______, _______, _______), */
 
+  
+        /*     LAYOUT_all //%% oneshot:bra_ru */
+        /* (_______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, S(_SLS), _______, _______, _______, _______, */
+        /*  _______,          _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______, */
+        /*  _______,          _______, S(_GRV),BRACKS_RU,_______, _GRV,             _______, _______, _______, _______, _______, _______,          _______, */
+        /*  _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______, */
+        /*  _______,                   _______, _______,        COM_SPC_RU,_______, _______,          _______, _______,          _______, _______, _______), */
+
+    
 }; 
 
 
@@ -127,13 +168,14 @@ bool process_record_user(uint16_t
   
 
   switch (keycode) {
-  case OSL_REF: return oneshot_process(record, __REF, 0);
+  case REF__: return oneshot_process(record, __REF, 0);
+  case BRA__: return oneshot_process(record, __BRA, 0);
   }
 
     if (1U << __OS & layer_state) {
         if (record->event.pressed) {
             switch(keycode) {
-            case APPSW:
+            case APPSW_:
                 if (record->event.pressed) {
                     if (get_mods() & MOD_BIT(KC_LSFT))
                         del_mods(MOD_BIT(KC_LSFT));
@@ -165,7 +207,7 @@ bool process_record_user(uint16_t
   
     switch (keycode) {
 
-        case OSMETA:
+        case SYSMETA_:
         if (record->event.pressed) {
             ostimer = timer_read();
             layer_on(__OS);
@@ -201,5 +243,31 @@ bool process_record_user(uint16_t
 
     }
 
+    if (record->event.pressed) {
+      switch (keycode) {
+      case PARENS:
+	SEND_STRING("()" SS_TAP(X_LEFT));
+	return false;
+      case BRACES:
+	SEND_STRING("{}" SS_TAP(X_LEFT));
+	/* layer_on(BRACES_LR); */
+	return false;
+      case BRCKS:
+	SEND_STRING("[]" SS_TAP(X_LEFT));
+	return false;
+      case BRCKSRU:
+	SEND_STRING("~`" SS_TAP(X_LEFT));
+	return false;
+      case ABRCKS:
+	SEND_STRING("<>" SS_TAP(X_LEFT));
+	return false;
+      case QUOTS:
+	SEND_STRING("''" SS_TAP(X_LEFT));
+	return false;
+      case DQUOTS:
+	SEND_STRING("\"\"" SS_TAP(X_LEFT));
+	return false;
+      }
+    }
     return 1;
 }
---
+++ keyboards/handwired/dactyl_manuform/6x7/keymaps/default/win.h
@@ -0,0 +1,138 @@
+#define SWITCH KC_LALT
+#define APPUP A(_UP)
+#define APPDN A(_DN)
+#define MTTASK A(_SPC)
+#define MTTASK2 KC_LWIN
+#define MTTASK3 C(A(S(_SPC)))
+#define TERM C(G(_1))
+#define XTERM SS_DOWN(X_LCTL) SS_DOWN(X_LGUI) "1" SS_UP(X_LGUI) SS_UP(X_LCTL)
+#define EMACS C(G(_2))
+#define XEMACS SS_DOWN(X_LCTL) SS_DOWN(X_LGUI) "2" SS_UP(X_LGUI) SS_UP(X_LCTL)
+#define BROWSER C(G(_3))
+#define XBROWSER SS_DOWN(X_LCTL) SS_DOWN(X_LGUI) "3" SS_UP(X_LGUI) SS_UP(X_LCTL)
+#define BROWSER2 C(G(_4))
+#define KILLAPP A(_F4)
+#define APKILL C(_W)
+#define FSCR G(_UP)
+#define FSCR1 A(S(_F12))
+#define LSCR G(_LT)
+#define RSCR G(_RT)
+
+// screens
+#define LMONITOR G(S(_LT))
+#define RMONITOR G(S(_LT))
+
+// tabs
+#define LTAB C(S(_TAB))
+#define RTAB C(_TAB)
+#define PTAB C(_Q)
+#define KILLTAB C(_W)
+#define MAGIC _F19
+
+// layout
+#define SPLITRT A(_F12)
+#define NEXTW C(A(_F12))
+#define PREVW C(A(S(_F12)))
+#define ZOOM C(_F12)
+#define DELCUR C(S(_F12))
+
+#define HELPKEY C(S(A(_H)))
+#define MX C(S(_P))
+#define MCOLON A(S(_SCL))
+#define PALETTE C(_L)
+
+#define ORECENT C(_P)
+#define OCWD A(_D)
+#define OALT C(A(_O))
+#define OHOME C(A(S(_O)))
+#define OCLIP C(_O)
+#define OTREE _F6
+#define ODIRED C(A(S(_E)))
+#define OFILE C(_O)
+#define ORC C(_COM)
+
+#define PATH A(S(_C))
+#define CWD C(A(S(_C)))
+
+#define UPD _F5
+#define SAVE C(_S)
+#define NENTRY A(_N)
+#define PENTRY A(_P)
+
+// ??? some substitutions for emacs standard keys
+#define E_CX _F18
+#define E_CC C(_F18)
+#define E_CV C(S(_F18))
+#define E_CZ C(A(S(_F18)))
+
+// motion
+#define CELLDN C(_F8)
+#define CELLUP C(_F9)
+#define RECENTER C(_F10)
+#define POPMARK _F17
+#define UNPOPMARK C(_F17) //:TD
+#define FSEARCH C(_F)
+#define BSEARCH C(S(_F))
+#define ALTSRCH _F3
+#define FINDNXT C(_G)
+#define FINDPRV C(S(_G))
+#define FINDAT C(S(_G))
+#define REPLACE C(S(_F))
+
+#define BOW_L C(_LT)
+#define BOW_R C(_RT)
+#define EOSW_R A(_RT)
+#define EOSW_L A(_LT)
+#define EOW_R C(A(S(_RT)))
+#define _EOL KC_END
+#define XEOL SS_TAP(X_END)
+#define XBOL SS_TAP(X_HOME)
+#define XBOW SS_DOWN(X_LCTL) SS_TAP(X_LEFT) SS_UP(X_LCTL)
+#define XEOW SS_DOWN(X_LCTL) SS_TAP(X_RIGHT) SS_UP(X_LCTL)
+#define _BOL A(_M)
+#define HARDBOL KC_HOME
+#define _EOF C(KC_END)
+#define _BOF C(KC_HOME)
+
+// editing
+#define DELETE S(KC_DELETE)
+#define CUT C(_X)
+/* #define COPY C(KC_INSERT) */
+#define COPY C(_C)
+#define PASTE S(KC_INSERT)
+#define PASTEI C(S(_V))
+#define ALL C(_A)
+#define UNDO C(_Z)
+#define REDO C(S(_Z))
+#define DUPL C(S(_D))
+#define COMMENT C(_SLS)
+#define DELBOL C(S(_BSP))
+#define DELEOL C(S(_DEL))
+#define DELBOW C(_BSP)
+#define DELEOW C(_DEL)
+#define DELEOSW C(A(S(_DEL)))
+#define SWAPUP A(_UP)
+#define SWAPDN A(_DN)
+#define NLBELOW C(A(_4))
+#define INDENT _TAB
+#define UNINDENT S(_TAB)
+#define SFTRT C(_RBR)
+#define SFTLT C(_LBR)
+
+// programming
+#define EVAL C(_ENT)
+#define EVALF S(_ENT) // forward
+#define EVALA _F12 // above
+#define EVALCUR C(A(_F11))
+#define _DEBUG _F5
+#define CLDEBUG S(_F5)
+#define REDEBUG C(S(_F5))
+#define NEWBELOW C(S(A(_B)))
+#define NEWABOVE C(S(A(_A)))
+#define DELCELL C(S(A(_D)))
+
+// some keys
+#define LABRC S(_COM)
+#define RABRC S(_DOT)
+#define QUE S(_SLS)
+#define RUQUE S(_7)
